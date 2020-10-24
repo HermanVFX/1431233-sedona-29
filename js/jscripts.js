b8051d219670e04eb76f629279895c0086b33681
HermanVFX <70486410+HermanVFX@users.noreply.github.com>
2020-10-24 18:05:28 +0300

    s

--- js/jscripts.js
+++ js/jscripts.js
@@ -10,6 +10,14 @@ var minus_chil = document.querySelector(".minus2");
 var par = form.querySelector(".name1_in");
 var chil = form.querySelector(".name2_in");
 
+var find_btn = document.querySelector(".show");
+
+var chek1 = document.querySelector(".check-box-1");
+var chek2 = document.querySelector(".check-box-2");
+var chek3 = document.querySelector(".check-box-3");
+
+var hotel = document.querySelector(".list-block-1")
+
 open_form_btn.addEventListener("click", function(evt) {
     form.classList.remove("form-visible");
     form.classList.toggle("form-non-visible");
@@ -40,3 +48,6 @@ plus_chil.addEventListener("click",function(evt) {
         chil.value ++;
     }
 });
+
+
+
